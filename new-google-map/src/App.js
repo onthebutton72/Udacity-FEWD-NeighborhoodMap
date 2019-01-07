b83d300391c23d536748840e1529aa8599729641
Jamie Martinez <martinejamie@hotmail.com>
2019-01-07 14:58:24 -0800

    Add Request for Four Square API data

--- new-google-map/src/App.js
+++ new-google-map/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './component/Map';
+import SquareAPI from './API';
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near: 'Austin, TX',
+      query: 'tacos',
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
